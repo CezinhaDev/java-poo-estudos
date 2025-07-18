c84726d5ef2ee43cef95d4a763ca71663e4bf994
CezinhaDev <cesarhenriquee04@gmail.com>
2025-07-18 09:38:16 -0300

    private e o modificador padrão

--- br/com/project/Filme.java
+++ br/com/project/Filme.java
@@ -4,9 +4,17 @@ public class Filme {
     String nome; 
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double SomaDasAvaliacoes;
+    private double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+    public double SomaDasAvaliacoes;
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    // esse é o construtor da classe Filme, que é chamado quando um objeto Filme é criado
+    
 
 
     // esse é o método que exibe a ficha técnica do filme, que sao os atributos
@@ -16,13 +24,13 @@ public class Filme {
     }
     // isso se chama método, que é uma ação que a classe pode realizar
     void avalia(double nota){
-        SomaDasAvaliacoes += nota;
+        somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
 
     }
 
     double pegaMedia(){
-        return SomaDasAvaliacoes / totalDeAvaliacoes;
+        return somaDasAvaliacoes / totalDeAvaliacoes;
     }
 }
 
--- br/com/project/Principal.java
+++ br/com/project/Principal.java
@@ -5,9 +5,6 @@ public class Principal {
         Filme meuFilme = new Filme();
         meuFilme.nome = "Poderoso Chefão";
         meuFilme.anoDeLancamento = 1972;
-        meuFilme.incluidoNoPlano = true;
-        meuFilme.SomaDasAvaliacoes = 9.2;
-        meuFilme.totalDeAvaliacoes = 10000;
         meuFilme.duracaoEmMinutos = 175;
 
         // Exibe a ficha técnica do filme
@@ -17,9 +14,9 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.SomaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
 
+        System.out.println(meuFilme.pegaMedia());
         System.out.println(meuFilme.pegaMedia());
     }
 }
